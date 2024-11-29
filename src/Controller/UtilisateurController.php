b0b689358ec2997508fa2285fd82c3d57a92f14a
sara <lsf.tdg@gmail.com>
2024-11-29 12:32:12 +0100

    connexion formulaire

--- src/Controller/UtilisateurController.php
+++ src/Controller/UtilisateurController.php
@@ -2,11 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Client;
 use App\Form\ConnexionType;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class UtilisateurController extends AbstractController
 {
@@ -18,10 +19,13 @@ class UtilisateurController extends AbstractController
         ]);
     }
     #[Route('/connexion', name: 'app_connexion')]
-    public function connexion(FormInterface $form): Response
+    public function connexion(): Response
     {
-        $form= $form(ConnexionType::class);
+        $client= new Client();
+        $formC=$this->createForm(ConnexionType::class, $client);
+        
         return $this->render('utilisateur/connexion.html.twig', [
+            'formC'=>$formC
         ]);
     }
 }
