b7d703f11deb26abb7feaacb1251af487334820c
sara <lsf.tdg@gmail.com>
2024-12-03 13:39:54 +0100

    firewall

--- src/Controller/UtilisateurController.php
+++ src/Controller/UtilisateurController.php
@@ -22,94 +22,146 @@ class UtilisateurController extends AbstractController
 {
  
     #[Route('/connexion', name: 'app_connexion')]
-    public function connexion(): Response
+    public function connexion(Request $request, EntityManagerInterface $entityManager): Response
     {
         $client= new Client();
         $formC=$this->createForm(ConnexionType::class, $client);
+        $formC->handlerequest($request);
+        if($formC->isSubmitted() && $formC->isValid()){
+            $data=$formC->getData();
+            $mail=$data['mail'];
+            $utilisateur=$entityManager->getRepository(Client::class)->findBy(['mail'=>$mail]);
+            if($utilisateur){
+                $mdpCorrect=$utilisateur['motDePasse'];
+                $mdp=$data('mdp');
+                if(password_verify($mdp,$mdpCorrect[0])){
+                    $session=$request->getSession($mail);
+                }
+                else{
 
+                    return $this->render('utilisateur/connexion.html.twig', [
+                        'formC'=>$formC,
+                        'message'=>"L'email ou le mot de passe est incorrect",
+                    ]);
+                }
+            }
+            else{
+                return $this->redirectToRoute('app_inscription',[
+                    'message'=>"Vous n'êtes pas encore inscrit",
+                ]);
+            };
+
+        }
+   
         return $this->render('utilisateur/connexion.html.twig', [
-            'formC'=>$formC
+            'formC'=>$formC,
+            // 'message'=>'',
         ]);
     }
+    #[Route('/test', name: 'app_test')]
+    public function test(): Response
+    {
+        return $this->render('utilisateur/test.html.twig', [
+        ]);
+    }
+    #[Route('/test2', name: 'app_test2')]
+    public function test2(): Response
+    {
+       
+
+        return $this->render('utilisateur/test2.html.twig', [
+        ]);
+    }
+
 
     #[Route('/inscription', name: 'app_inscription')]
     public function inscription(Request $request, EntityManagerInterface $entityManager, MailerInterface $mailer): Response
     {
         $formI=$this->createForm(InscriptionType::class);  
-        
+        $formI->handleRequest($request);
 
-        if($formI->isSubmitted() && $formI->isValid()){
-            $formI->handleRequest($request);
+
+        if($formI->isSubmitted() && $formI->isValid()){                 
             $data=$formI->getData();
             $leMail=$data['mail'];
-            // $presence=$entityManager->getRepository(Client::class)->findBy['mail'];
 
+            $presence=$entityManager->getRepository(Client::class)->findBy(["mail"=>$leMail]);    
 
-            $client= new Client();
-            $adresse= new Adresse();
-            $date= new DateTime('now');
-            $mdp=password_hash($data['mdp'],PASSWORD_DEFAULT);
-            $referenceClient=substr($data['prenom'],0,2).substr($data['nom'],0,2).random_int(0,10000);
+            if(!isset($presence)){ 
+                $client= new Client();
+                $adresse= new Adresse();
+                $date= new DateTime('now');
+                $mdp=password_hash($data['mdp'],PASSWORD_DEFAULT);
+                $referenceClient=substr($data['prenom'],0,2).substr($data['nom'],0,2).random_int(0,10000);
 
-            $client->setNom($data['nom']);
-            $client->setPrenom($data['prenom']);
-            $client->setMail($data['mail']);
-            $client->setNumeroTelephone($data['numeroTelephone']);
-            $client->setMotDePasse($mdp);
-            $client->setNom($data['nom']);
-            $client->setDerniereConnexion($date);
-            $client->setReferenceClient($referenceClient);
+                $client->setNom($data['nom']);
+                $client->setPrenom($data['prenom']);
+                $client->setMail($data['mail']);
+                $client->setNumeroTelephone($data['numeroTelephone']);
+                $client->setMotDePasse($mdp);
+                $client->setNom($data['nom']);
+                $client->setDerniereConnexion($date);
+                $client->setReferenceClient($referenceClient);
 
-            $adresse->setPersonne($client);
-            $adresse->setFacturation(0);
-            $adresse->setLivraison(0);
-            $adresse->setActif(1);
-            $adresse->setAdresse($data['adresse']);
+                $adresse->setPersonne($client);
+                $adresse->setFacturation(0);
+                $adresse->setLivraison(0);
+                $adresse->setActif(1);
+                $adresse->setAdresse($data['adresse']);
 
-            $adresse->setAdresse($data['adresse']);
-            $adresse->setVille($data['ville']);
-            $adresse->setCp($data['cp']);
+                $adresse->setAdresse($data['adresse']);
+                $adresse->setVille($data['ville']);
+                $adresse->setCp($data['cp']);
 
-            $manager->persist($client);
-            $manager->persist($adresse);
-            $manager->flush();
-            
-            $mail = (new TemplatedEmail())
-            ->from('greenVillage@example.com')
-            ->to(new Address($data['mail']))
-            ->subject('Confirmer votre compte GreenVillage')
+                $entityManager->persist($client);
+                $entityManager->persist($adresse);
+                $entityManager->flush();
 
-            // path of the Twig template to render
-            ->htmlTemplate('mail/mail.html.twig')
+                $session=$request->setSession($data['mail']);
 
-            // change locale used in the template, e.g. to match user's locale
-            ->locale('de')
+                $mail = (new TemplatedEmail())
+                ->from('greenVillage@example.com')
+                ->to(new Address($data['mail']))
+                ->subject('Confirmer votre compte GreenVillage')
 
-            // pass variables (name => value) to the template
-            ->context([
-                'expiration_date' => new \DateTime('+7 days'),
-                'username' => $data['mail'],
-                'name'=>$data['prenom'],
+                // path of the Twig template to render
+                ->htmlTemplate('mail/mail.html.twig')
 
-            ])
-        ;
-        $mailer->send($mail);
-            // $mail=(new Email())
-            // ->from('GreenVillage@gmail.com')
-            // ->to($data['mail'])
-            // //->cc('cc@example.com')
-            // //->bcc('bcc@example.com')
-            // //->replyTo('fabien@example.com')
-            // //->priority(Email::PRIORITY_HIGH)
-            // ->subject('Confirmation de votre compte Green Village')
-            // ->text("Veuillez confirmer votre compte en cliquant sur le lien ci-dessous s'il vous plaît")
-            // // ->html('<p>See Twig integration for better HTML integration!</p>');
-            // ;
-            // $mailer->send($mail);
+                // change locale used in the template, e.g. to match user's locale
+                ->locale('de')
 
-            return $this->redirectToRoute('utilisateur/test.html.twig',[
-                
-            ]);
+                // pass variables (name => value) to the template
+                ->context([
+                    'expiration_date' => new \DateTime('+7 days'),
+                    'username' => $data['mail'],
+                    'name'=>$data['prenom'],
+
+                ])
+            ;
+            $mailer->send($mail);
+                // $mail=(new Email())
+                // ->from('GreenVillage@gmail.com')
+                // ->to($data['mail'])
+                // //->cc('cc@example.com')
+                // //->bcc('bcc@example.com')
+                // //->replyTo('fabien@example.com')
+                // //->priority(Email::PRIORITY_HIGH)
+                // ->subject('Confirmation de votre compte Green Village')
+                // ->text("Veuillez confirmer votre compte en cliquant sur le lien ci-dessous s'il vous plaît")
+                // // ->html('<p>See Twig integration for better HTML integration!</p>');
+                // ;
+                // $mailer->send($mail);
+
+                return $this->redirectToRoute('app_test');
+            }
+            else{
+                $dejaCompte='Vous avez déjà un compte Green Village.';
+                return $this->redirectToRoute('app_connexion',[
+                    'dejaCompte'=>$dejaCompte,
+                    'mail'=>$leMail,
+                ]);
+
+            };
         }
         return $this->render('utilisateur/inscription.html.twig', [
             'formI'=>$formI,
