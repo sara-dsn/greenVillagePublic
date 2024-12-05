92565d4a96187c8a0b8a649666923fc971bee864
sara-dsn <sabr.tdg@gmail.com>
2024-12-05 19:07:36 +0100

    debug

--- assets/app.js
+++ assets/app.js
@@ -1,5 +1,5 @@
 // import { registerReactControllerComponents } from '@symfony/ux-react';
-// import './bootstrap.js';
+//* import './bootstrap.js';
 /*
  * Welcome to your app's main JavaScript file!
  *
